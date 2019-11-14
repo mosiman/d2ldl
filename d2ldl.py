7e242992ab5588308d698d9c6836dac45a6704ee
Dillon Chan <dillonchan97@gmail.com>
2019-11-14 10:34:18 -0500

    Clean up some stuff, add requirements

--- d2ldl.py
+++ d2ldl.py
@@ -1,49 +1,14 @@
+#!/bin/python3.7
+
 from requests_html import HTMLSession 
 from bs4 import BeautifulSoup
 from getpass import getpass, getuser
 import json
 import re
-
 import os
 
-print("Welcome to d2ldl: download shit from Learn")
-print("Logging you in...")
 
-session = HTMLSession(mock_browser=True)
-
-r = session.get('https://learn.uwaterloo.ca/d2l/login?&noredirect=1')
-
-user = input('WatIAm ID: ')
-pw = getpass()
-
-payload = {'nordirect': 1, 'loginPath': '/d2l/login', 'UserName': user, 'Password' : pw}
-
-r = session.post('https://learn.uwaterloo.ca/d2l/lp/auth/login/login.d2l', data=payload, allow_redirects=False)
-
-# sessionVals = r.cookies.get_dict() 
-
-r = session.get('https://learn.uwaterloo.ca/d2l/home')
-
-soup = BeautifulSoup(r.content, 'html.parser')
-
-courses = session.get('https://learn.uwaterloo.ca/d2l/lp/courseSelector/6606/InitPartial?_d2l_prc%24headingLevel=2&_d2l_prc%24scope=&_d2l_prc%24hasActiveForm=false&isXhr=true&requestId=3')
-
-if courses.text.find('while(1);') == 0:
-    coursesjson = courses.text[len('while(1);'):]
-
-coursesdict = json.loads(coursesjson)
-
-courseselectorsoup = BeautifulSoup(coursesdict['Payload']['Html'], 'html.parser')
-
-courseselectoritems = courseselectorsoup.find_all('div', attrs={'class':'d2l-course-selector-item'})
-
-
-print("You are taking: ")
-for course in courseselectoritems:
-    print(course.find('a').text)
-    print('>>> with unit id: ' + course.get('data-org-unit-id'))
-
-unitid = input('Download which unit id? ')
+# buncha functions and shit, probably move out to another file if this gets any bigger
 
 
 def extractItemID(href):
@@ -56,30 +21,6 @@ def extractFnameFromContentDisposition(cd):
     fname = fnamething.split('=')[1].split('"')[1]
 
     return fname
-
-foldername = input('folder name? ')
-
-folderExists = os.path.isdir(foldername)
-
-if folderExists:
-    print("Folder exists.")
-else:
-    print("Creating folder...")
-    os.mkdir(foldername)
-
-# doesn't go into folders (e.g. Ex2 in STA332)
-# <a class="d2l-link" href="/d2l/le/content/478148/Home?itemIdentifier=D2L.LE.Content.ContentObject.ModuleCO-2713629">Ex2</a>
-# this link is accessed FROM TOC/Exercise/Ex2/ex2 (the pdf file)
-# this link brings us to the Ex2 folder
-# use this to recurse?
-# how to access from TOC?
-# Look for <li> with a `data-key` and `class : d2l-le-TreeAccordionItem`
-#   Then find the ones with `D2l.LE.Content.ContentObject.Module` in their id or data-key.
-#   The data key is the link to the folder
-
-
-print("Downloading...")
-
 
 def downloadFromFolder(page, foldername):
     """
@@ -106,14 +47,73 @@ def downloadFromFolder(page, foldername):
             open(os.path.join(foldername, fname), 'wb').write(d.content)
 
 
+# ==================================
+# ======= the goodie goods =========
+# ==================================
+
+print("Welcome to d2ldl: download shit from Learn")
+print("Logging you in...")
+
+session = HTMLSession(mock_browser=True)
+
+r = session.get('https://learn.uwaterloo.ca/d2l/login?&noredirect=1')
+
+user = input('WatIAm ID: ')
+pw = getpass()
+
+payload = {'nordirect': 1, 'loginPath': '/d2l/login', 'UserName': user, 'Password' : pw}
+
+# Log in via a session, values are sent in headers as required from here on.
+r = session.post('https://learn.uwaterloo.ca/d2l/lp/auth/login/login.d2l', data=payload, allow_redirects=False)
+# sessionVals = r.cookies.get_dict() 
+
+
+# Go to the home page
+r = session.get('https://learn.uwaterloo.ca/d2l/home')
+soup = BeautifulSoup(r.content, 'html.parser')
+
+# find the term ID (is that what it is?)
+# termID is needed to find the courses for this term.
+# the 'calendar' URL just so happens to have this term ID
+calendarURL = soup.find('a', href=lambda href: href and 'calendar' in href)
+termID = calendarURL.get('href').split('/')[-1]
+
+
+# Parse the courses data
+courses = session.get(f'https://learn.uwaterloo.ca/d2l/lp/courseSelector/{termID}/InitPartial')
+if courses.text.find('while(1);') == 0:
+    coursesjson = courses.text[len('while(1);'):]
+coursesdict = json.loads(coursesjson)
+courseselectorsoup = BeautifulSoup(coursesdict['Payload']['Html'], 'html.parser')
+courseselectoritems = courseselectorsoup.find_all('div', attrs={'class':'d2l-course-selector-item'})
+
+
+print("You are taking: ")
+for course in courseselectoritems:
+    print(course.find('a').text)
+    print('    with unit id: ' + course.get('data-org-unit-id'))
+
+unitid = input('Download which unit id? ')
+foldername = input('folder name? ')
+folderExists = os.path.isdir(foldername)
+if folderExists:
+    print("Folder exists.")
+else:
+    print("Creating folder...")
+    os.mkdir(foldername)
+
+print("Downloading...")
+
 contentlinkTOC = f'https://learn.uwaterloo.ca/d2l/le/content/{unitid}/Home?itemIdentifier=TOC'
 contentlinkItemIDBase = f'https://learn.uwaterloo.ca/d2l/le/content/{unitid}/Home?itemIdentifier='
 
+# Go to table of contents
 r = session.get(contentlinkTOC)
-
 soup = BeautifulSoup(r.content, 'html.parser')
 
 # See if I can traverse all the folders on the page.
+# TODO: recursively build content structure
+# right now: only looks at TOC and immediate modules
 
 lis = soup.find_all('li', attrs={'data-key': True, 'class': 'd2l-le-TreeAccordionItem'})
 maybefolders = [x for x in lis if 'D2L.LE.Content.ContentObject.Module' in x.get('id')]
@@ -132,11 +132,3 @@ for folder in maybefolders:
     folderlink = contentlinkItemIDBase + folder.get('data-key')
     print(f'Downloading from {folderTitle}...')
     downloadFromFolder(folderlink, os.path.join(foldername, folderTitle))
-
-
-
-
-
-
-
-
